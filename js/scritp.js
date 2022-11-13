5563ab3d806c1c7d519c7a4f4ccb19a11d5d4c72
Murod Xolmajonov <murodx3363@gmail.com>
2022-11-12 22:30:49 -1000

    i ended lalasia web-site )

---
+++ js/scritp.js
@@ -0,0 +1,39 @@
+console.log("iwladi");
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-box');
+    const navLinks = document.querySelectorAll('.nav-box li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+
+// swiper //
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
